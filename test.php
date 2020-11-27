f6734a1a227ce37bae8bcb29d034083e3ce235e9
Jean Paul Noel BADIANE <jpnbadiane@gmail.com>
2020-11-27 13:55:50 +0100

    exemple usage twig

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, []);
+
+echo $twig->render('test.html', [
+    'name' => 'Fabien',
+    "items" => [['name' => 'bob' ], ['name' => 'ruby' ]] 
+]);
+
