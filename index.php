60417261c24dd4623cec72eb359a1db7bfd8988a
Awea <aweaoftheworld@gmail.com>
2020-12-02 15:47:51 +0100

    More understandable index.php templates/index.html example

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php
-require_once "vendor/autoload.php";
+require_once 'vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader, []);
 
 echo $twig->render('index.html', [
-    'menu' => 'Menu Classic',
-    "items" => [['menu' => 'Menu Bacon' ], ['menu' => 'Menu Big' ], ['menu' => 'Menu Chicken' ], ['menu' => 'Menu Fish' ], ['menu' => 'Menu Double Steak' ]] 
-    ]); 
-
-echo $twig->render('index.html', [
-        'burger' => 'Classic',
-        "items" => [['burger' => 'Bacon' ], ['burger' => 'Big' ], ['burger' => 'Chicken' ], ['burger' => 'Fish' ], ['burger' => 'Double Steak' ]] 
-        ]);     
+    'myNavbar' => [
+        [ 'href' => '#menus', 'name' => 'Menus'],
+        [ 'href' => '#burgers', 'name' => 'Burgers' ],
+        [ 'href' => '#salades', 'name' => 'Salades' ],
+        [ 'href' => '#snacks', 'name' => 'Snacks' ],
+        [ 'href' => '#boissons', 'name' => 'Boissons' ],
+        [ 'href' => '#desserts', 'name' => 'Desserts' ],
+        [ 'href' => '#panier', 'name' => 'Panier' ]
+    ],
+    // 'menus' => [
+    //     [
+    //         'image' => 'images/m1.png',
+    //         'price' => '8,90€',
+    //         'name' => 'Menu Classic',
+    //         'description' => 'Sandwich: Burger, Salade, Tomate, Cornichon + Frites + Boisson'
+    //     ],
+    //     [
+    //         'image' => 'images/m1.png',
+    //         'price' => '8,90€',
+    //         'name' => 'Menu Classic',
+    //         'description' => 'Sandwich: Burger, Salade, Tomate, Cornichon + Frites + Boisson'
+    //     ]
+    // ]
+]);
