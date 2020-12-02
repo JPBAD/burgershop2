e90b607941073cee390330174ed37b50c9a767be
Jean Paul Noel BADIANE <jpnbadiane@gmail.com>
2020-12-02 16:18:07 +0100

    dynamisme menu

--- index.php
+++ index.php
@@ -14,18 +14,42 @@ echo $twig->render('index.html', [
         [ 'href' => '#desserts', 'name' => 'Desserts' ],
         [ 'href' => '#panier', 'name' => 'Panier' ]
     ],
-    // 'menus' => [
-    //     [
-    //         'image' => 'images/m1.png',
-    //         'price' => '8,90€',
-    //         'name' => 'Menu Classic',
-    //         'description' => 'Sandwich: Burger, Salade, Tomate, Cornichon + Frites + Boisson'
-    //     ],
-    //     [
-    //         'image' => 'images/m1.png',
-    //         'price' => '8,90€',
-    //         'name' => 'Menu Classic',
-    //         'description' => 'Sandwich: Burger, Salade, Tomate, Cornichon + Frites + Boisson'
-    //     ]
-    // ]
+    'menus' => [
+         [
+            'image' => 'images/m1.png',
+             'price' => '8,90€',
+            'name' => 'Menu Classic',
+            'description' => 'Sandwich: Burger, Salade, Tomate, Cornichon + Frites + Boisson'
+        ],
+        [
+            'image' => 'images/m2.png',
+            'price' => '9,5€',
+            'name' => 'Menu Bacon',
+            'description' => 'Sandwich: Burger, Fromage, Bacon, Salade, Tomate + Frites + Boisson'
+        ],
+        [
+            'image' => 'images/m3.png',
+            'price' => '10,90€',
+            'name' => 'Menu Big',
+            'description' => 'Sandwich: Double Burger, Fromage, Cornichon, Salade + Frites + Boisson'
+        ],
+        [
+            'image' => 'images/m4.png',
+            'price' => '10,90€',
+            'name' => 'Menu Chicken',
+            'description' => 'Poulet Frit, Tomate, Salade, Mayonnaise + Frites + Boisson'
+        ],
+        [
+            'image' => 'images/m2.png',
+            'price' => '9,90€',
+            'name' => 'Menu Fish',
+            'description' => 'Poisson, Salade, Mayonnaise, Cornichon + Frites + Boisson'
+        ],
+        [  
+            'image' => 'images/m2.png',
+            'price' => '11,90€',
+            'name' => 'Menu Double Steak',
+            'description' => 'Sandwich: Double Burger, Fromage, Bacon, Salade, Tomate + Frites + Boisson'
+        ]
+    ]
 ]);
