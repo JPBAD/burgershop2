323cfd47ad5821c2e178176da65d8d64a29915ee
Awea <aweaoftheworld@gmail.com>
2020-12-09 15:28:09 +0100

    Improve/begin cart and add a modal to display it

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 require_once 'vendor/autoload.php';
 
+creationPanier();
+
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader, []);
 
-try { 
+try {
     $db = new \PDO(
         "mysql:host=localhost;dbname=burger_shop",
         "root",
         "root"
     );
 
-} catch (PDOException $e) { 
+} catch (PDOException $e) {
     print $e->getMessage();
 }
 
@@ -41,34 +43,36 @@ $dessertsQuery = $db->prepare('SELECT * FROM products where type= "dessert"');
 $dessertsQuery->execute();
 $desserts=$dessertsQuery->fetchAll();
 
-
-
-echo $twig->render('index.html', [
-    'myNavbar' => [
-        [ 'href' => '#menus', 'name' => 'Menus'],
-        [ 'href' => '#burgers', 'name' => 'Burgers' ],
-        [ 'href' => '#salades', 'name' => 'Salades' ],
-        [ 'href' => '#snacks', 'name' => 'Snacks' ],
-        [ 'href' => '#boissons', 'name' => 'Boissons' ],
-        [ 'href' => '#desserts', 'name' => 'Desserts' ],
-        [ 'href' => '#panier', 'name' => 'Panier' ]
-    ],
-    'menus' => $menus, //dynamisme menus
-    'burgers' =>$burgers,
-    'salades' =>$salades, 
-    'snacks' =>$snacks, 
-    'boissons' =>$boissons, 
-    'desserts' =>$desserts, 
-]);
-
+// Renommer toutes les occurences de panier en cart, on ne fait pas de franglais :3
 function creationPanier(){
     session_start();
-    /* Initialisation du panier */
-    $_SESSION['panier'] = array();
-    /* Subdivision du panier */
-    $_SESSION['panier']['name'] = array();
-    $_SESSION['panier']['quantity'] = array();
-    $_SESSION['panier']['price'] = array();
+
+    if (!isset($_SESSION['panier'])){
+        $_SESSION['panier'] = [
+            'products' => [],
+            'total' => 0.0
+        ];
+    }
+}
+
+function addToCart($product_id){
+    // À faire : Utiliser le product_id envoyé pour faire une requête dans la DB et
+    // ajouter le produit au panier
+    $product = "resultat requête pour recup produit";
+
+    if (!isset($_SESSION['panier']['products'][$product_id])){
+        $_SESSION['panier']['products'][$product_id] = [
+            'name' => 'product name', // $product['name']
+            'price' => '10,00 €', // $product['price'] / 100 tranformé en €
+            'quantity' => 1 // quantité par défaut a 1 produit
+        ]
+    } else {
+        $_SESSION['panier']['products'][$product_id]['quantity'] += 1;
+    }
+}
+
+if (isset($_GET['action']) && $_GET['action'] == 'addToCart' && isset($_GET['product_id'])){
+    addToCart($_GET['product_id']);
 }
 
 function montantGlobal(){
@@ -79,4 +83,21 @@ function montantGlobal(){
     }
     return $total;
  }
-?>
+
+echo $twig->render('index.html', [
+    'myNavbar' => [
+        [ 'href' => '#menus', 'name' => 'Menus'],
+        [ 'href' => '#burgers', 'name' => 'Burgers' ],
+        [ 'href' => '#salades', 'name' => 'Salades' ],
+        [ 'href' => '#snacks', 'name' => 'Snacks' ],
+        [ 'href' => '#boissons', 'name' => 'Boissons' ],
+        [ 'href' => '#desserts', 'name' => 'Desserts' ]
+    ],
+    'cart' => $_SESSION['panier'],
+    'menus' => $menus, //dynamisme menus
+    'burgers' =>$burgers,
+    'salades' =>$salades,
+    'snacks' =>$snacks,
+    'boissons' =>$boissons,
+    'desserts' =>$desserts,
+]);
